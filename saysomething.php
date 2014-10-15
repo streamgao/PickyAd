b58460e82fc17eefef78b33976c2fb436122604b
Stream <gaoxi1992@hotmail.com>
2014-10-15 12:30:16 -0400

    ASDF

--- saysomething.php
+++ saysomething.php
@@ -12,14 +12,14 @@
 <div class="subcontainer">
     <br> <br>
     <h2>
-        Hi! say something about the game. What do you think of it? Any ideas to make it more interesting or useful?</h2>
-    <br>
+        Hi! say something about the game. What do you think of it? Any ideas to make it more interesting or useful?
+    </h2> <br>
 
     <form action="questions.php" method="get">
         Name: <input type="text" name="name" value="name"><br> <br>
         <textarea rows="20" cols="90" name="comment" value="comment">
             Enter your comment here...</textarea>    <br>
-        <input type="submit"  name="submit" value="submit">
+        <input type="submit"  class="buttonClass" name="submit" value="submit">
     </form>
 
     <br> <br><br> <br><br> <br><br> <br><br>
