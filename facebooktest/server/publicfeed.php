40e82e662ed8a5aab08a56f6ad7124cbe9919d53
Stream <gaoxi1992@hotmail.com>
2014-11-16 22:32:03 -0500

    adf

--- facebooktest/server/publicfeed.php
+++ facebooktest/server/publicfeed.php
@@ -16,6 +16,7 @@ require_once( 'Facebook/FacebookRequestException.php' );
 require_once( 'Facebook/FacebookAuthorizationException.php' );
 require_once( 'Facebook/GraphObject.php' );
 require_once( 'Facebook/GraphUser.php' );
+require_once( 'Facebook/FacebookJavaScriptLoginHelper.php' );
 
 use Facebook\FacebookSession;
 use Facebook\FacebookRedirectLoginHelper;
@@ -26,6 +27,7 @@ use Facebook\FacebookRequestException;
 use Facebook\FacebookAuthorizationException;
 use Facebook\GraphObject;
 use Facebook\GraphUser;
+use Facebook\FacebookJavaScriptLoginHelper;
 
 
 session_start();
@@ -46,13 +48,13 @@ try {
     FacebookSession::setDefaultApplication('770707736335720', '15d79634f7b439545f0d60328dea3998');
     echo "successful";
 }catch (Exception $e){
-    echo "session error";
+    echo "session create error";
 }
 
-SignedRequest $signedRequest = null;
-
 try {
-    $session = new FacebookSession( $access_token, $signedRequest );
+    //$session = new FacebookSession( $access_token );
+    $helper = new FacebookJavaScriptLoginHelper();
+    $session = $helper->getSession();
     echo "session";
     echo $session;
 }
