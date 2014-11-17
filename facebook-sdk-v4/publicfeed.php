56a9a1fd7356638be3ce1a0785d4345271d5d7f7
Stream <gaoxi1992@hotmail.com>
2014-11-17 10:22:42 -0500

    ad

--- facebook-sdk-v4/publicfeed.php
+++ facebook-sdk-v4/publicfeed.php
@@ -5,32 +5,38 @@
  * Date: 11/17/14
  * Time: 2:52 AM
  */
-//
+
 //require_once __DIR__ . "/facebook-php-sdk-v4-4.0-dev/src/Facebook/FacebookSession.php";
 //require_once __DIR__ . "/facebook-php-sdk-v4-4.0-dev/src/Facebook/FacebookRequest.php";
 //require_once __DIR__ . "/facebook-php-sdk-v4-4.0-dev/src/Facebook/FacebookResponse.php";
 //require_once __DIR__ . "/facebook-php-sdk-v4-4.0-dev/src/Facebook/GraphObject.php";
 //require_once __DIR__ . "/facebook-php-sdk-v4-4.0-dev/src/Facebook/GraphUser.php";
 
+require_once('facebook-php-sdk-v4-4.0-dev/src/Facebook/FacebookSession.php');
+require_once('facebook-php-sdk-v4-4.0-dev/src/Facebook/FacebookRequest.php');
+require_once('facebook-php-sdk-v4-4.0-dev/src/Facebook/FacebookResponse.php');
+require_once('facebook-php-sdk-v4-4.0-dev/src/Facebook/GraphObject.php');
+require_once('facebook-php-sdk-v4-4.0-dev/src/Facebook/GraphUser.php');
+
 use Facebook\FacebookSession;
-//use Facebook\FacebookRequest;
-//use Facebook\FacebookResponse;
-//use Facebook\GraphObject;
-//use Facebook\GraphUser;
+use Facebook\FacebookRequest;
+use Facebook\FacebookResponse;
+use Facebook\GraphObject;
+use Facebook\GraphUser;
 
 
 session_start();
 
-//if($_SERVER['REQUEST_METHOD'] === "POST" ){
-//    $method = $_POST['method'];
-//}else{
-//    $method = $_GET['method'];
-//}
-//
-////$access_token = $_GET['access_token'];
-//$access_token = $_REQUEST['code'];
-//echo $access_token;
-//
+if($_SERVER['REQUEST_METHOD'] === "POST" ){
+    $method = $_POST['method'];
+}else{
+    $method = $_GET['method'];
+}
+
+//$access_token = $_GET['access_token'];
+$access_token = $_REQUEST['code'];
+echo $access_token;
+
 echo $_SESSION["color"];
 
 echo "get session!";
