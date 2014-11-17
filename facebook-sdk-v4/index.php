17282f09fcfde8e882ebc1d1adc7bc6480bb7f10
Stream <gaoxi1992@hotmail.com>
2014-11-17 04:24:45 -0500

    adv

--- facebook-sdk-v4/index.php
+++ facebook-sdk-v4/index.php
@@ -13,6 +13,11 @@ $mysql_password		= '';
 $mysql_db_name		= 'test';
 
 require_once __DIR__ . "/facebook-php-sdk-v4-4.0-dev/autoload.php"; //include autoload from SDK folder
+require_once __DIR__ . "/facebook-php-sdk-v4-4.0-dev/src/Facebook/FacebookSession.php";
+require_once __DIR__ . "/facebook-php-sdk-v4-4.0-dev/src/Facebook/FacebookRequest.php";
+require_once __DIR__ . "/facebook-php-sdk-v4-4.0-dev/src/Facebook/FacebookResponse.php";
+require_once __DIR__ . "/facebook-php-sdk-v4-4.0-dev/src/Facebook/GraphObject.php";
+require_once __DIR__ . "/facebook-php-sdk-v4-4.0-dev/src/Facebook/GraphUser.php";
 
 //import required class to the current scope
 use Facebook\FacebookSession;
