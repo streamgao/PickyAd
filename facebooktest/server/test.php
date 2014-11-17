f94a4267733b76de5738625fa82303044776bd8b
Stream <gaoxi1992@hotmail.com>
2014-11-17 00:38:26 -0500

    adf

--- facebooktest/server/test.php
+++ facebooktest/server/test.php
@@ -36,8 +36,8 @@ if($_SERVER['REQUEST_METHOD'] === "POST" ){
     $method = $_GET['method'];
 }
 
-$access_token = $_GET['access_token'];
-//$access_token = $_REQUEST["access_token"];
+//$access_token = $_GET['access_token'];
+$access_token = $_REQUEST["access_token"];
 echo $access_token;
 
 
