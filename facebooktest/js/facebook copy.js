fd77832e4ce6c648c804c5640cb21929877876bc
Stream <gaoxi1992@hotmail.com>
2014-11-20 17:10:45 -0500

    D

--- facebooktest/js/facebook copy.js
+++ facebooktest/js/facebook copy.js
@@ -1,54 +1,41 @@
 
- // This is called with the results from from FB.getLoginStatus().
   function statusChangeCallback(response) {
     console.log('statusChangeCallback');
     console.log(response);
     console.log(response['authResponse']['accessToken']);
 
-    // The response object is returned with a status field that lets the
-    // app know the current login status of the person.
-    // Full docs on the response object can be found in the documentation
-    // for FB.getLoginStatus().
     if (response.status === 'connected') {
-      // Logged into your app and Facebook.
-      //or response.authResponse.accessToken
       var AccessToken=response['authResponse']['accessToken'];
       var uid = response.authResponse.userID;
       var accessToken = response.authResponse.accessToken;
       publicFeed( accessToken );
-
     } else if (response.status === 'not_authorized') {
       // The person is logged into Facebook, but not your app.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into this app.';
+      document.getElementById('status').innerHTML = 'Please log ' + 'into this app.';
     } else {
       // The person is not logged into Facebook, so we're not sure if
       // they are logged into this app or not.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into Facebook.';
+      document.getElementById('status').innerHTML = 'Please log ' +  'into Facebook.';
     }
   }//statusChangeCallback
 
-  // This function is called when someone finishes with the Login Button.
-  // See the onlogin handler attached to it in the sample code below.
-  function checkLoginState() {
+  //When someone finishes with the Login Button.
+  //See the onlogin handler attached to it in the sample code below.
+      function checkLoginState() {
+        FB.getLoginStatus(function(response) {
+            console.log("getLoginStatus" + response);
+          statusChangeCallback(response);
+        });
+      }//checkLoginState
 
-
-
-    FB.getLoginStatus(function(response) {
-        console.log("getLoginStatus" + response);
-      statusChangeCallback(response);
-    });
-  }
-
-    window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '770707736335720',
-    cookie     : true,  // enable cookies to allow the server to access
+      window.fbAsyncInit = function() {
+      FB.init({
+        appId      : '770707736335720',
+        cookie     : true,  // enable cookies to allow the server to access
                         // the session
-    xfbml      : true,  // parse social plugins on this page
-    version    : 'v2.2' // use version 2.1
-  }); //init
+        xfbml      : true,  // parse social plugins on this page
+        version    : 'v2.2' // use version 2.1
+      }); //init
 
   // Now that we've initialized the JavaScript SDK, we call
   // FB.getLoginStatus().  This function gets the state of the
@@ -60,34 +47,23 @@
   //    your app or not.
   // These three cases are handled in the callback function.
 
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);   });
-  };
+     FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);   });
+     };
 
   // Load the SDK asynchronously
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
 
 
-  function publicFeed( accessToken ) {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me/home','get',
-      function(response) {
-      console.log('p Successful login for: ' + response);
-          var jsonD =  {data:JSON.stringify(response)};
-          console.log("jsonD:", response);
-
-      document.getElementById('status').innerHTML =
-        'Thanks for logging in, ' + response+ '!';
-        //findUserFriends();
-      });
-
-      FB.api('/me/post',
+    function publicFeed( accessToken ) {
+        console.log('Welcome!  Fetching your information.... ');
+        FB.api('/me/post',
           function(response) {
               console.log('p8 Successful login for: ' + response);
               var jsonD =  {data:JSON.stringify(response)};
@@ -96,16 +72,16 @@
               document.getElementById('status').innerHTML =
                   'Thanks for logging in, ' + response+ '!';
               //findUserFriends();
-      });
+        });
 
-      var body = 'Reading JS SDK documentation';
-      FB.api('/me/feed', 'post', { message: body }, function(response) {
-          if (!response || response.error) {
-              alert('Error occured');
-          } else {
+        var body = 'Reading JS SDK documentation';
+          FB.api('/me/feed', 'post', { message: body }, function(response) {
+            if (!response || response.error) {
+                  alert('Error occured');
+            } else {
               alert('Post ID: ' + response.id);
-          }
-      });
+            }
+        });
 
 	}//publicfeed
 
@@ -121,6 +97,7 @@
         request.setRequestHeader("Content-Type", "application/json"); 
         request.send(JSON.stringify(data));
   }
+ //</editor-fold>
 
 
 
