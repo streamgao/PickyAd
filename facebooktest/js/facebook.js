7bae6c9159aa4218bef7f6bfcea1d0dd07341088
Stream <gaoxi1992@hotmail.com>
2014-11-20 19:11:32 -0500

    adcvxz

--- facebooktest/js/facebook.js
+++ facebooktest/js/facebook.js
@@ -62,11 +62,11 @@ function publicFeed( accessToken ) {
             console.log("jsonD:", response);
     });*/
 
-    console.log(accessToken);
     $.get('https://graph.facebook.com/me/feed?access_token='+accessToken, function (data) {  //request data
         //var json = JSON.parse(data);
         console.log("http/feeds2"+data);
         console.log(data);
+        console.log(data[0]);
         console.log(data[0][0]);
         console.log(data[0][0]['created_time']);
         console.log(data[0][0].created_time);
