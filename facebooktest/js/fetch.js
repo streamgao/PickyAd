ebbd88fbaa4c0c0e0fa00ca7c830a7399c10e817
streamna <gaoxi1992@hotmail.com>
2014-11-22 16:50:51 -0500

    g

--- facebooktest/js/fetch.js
+++ facebooktest/js/fetch.js
@@ -4,8 +4,10 @@
 
 function now(){
     this.now=new Date();
-//  this.year= this.now.getFullYear(); this.month= this.now.getMonth()+1; this.date= this.now.getDate();
-    this.today = this.now.getFullYear() + "-" + this.now.getMonth()+1 + "-" + this.now.getDate();
+//  this.year= this.now.getFullYear();
+// this.date= this.now.getDate();
+    this.month= this.now.getMonth()+1;
+    this.today = this.now.getFullYear() + "-" + this.month + "-" + this.now.getDate();
 }
 
 function publicFeed( accessToken ) {
@@ -17,21 +19,18 @@ function publicFeed( accessToken ) {
      console.log("jsonD:", response);
      });*/
     $.get('https://graph.facebook.com/me/feed?access_token='+accessToken, function (feeds) {  //request data
-        //console.log("http/feeds2"+data);
-        console.log(feeds);
         console.log(feeds.data);
-        console.log(feeds.data.length);
+
         var feed = feeds.data;
 
         var today = new now();
         console.log( today.today );
 
         for(var i=0; i< feed.length; i++){
-
-            if( feed[i]['message']!=null ){
-                console.log(i+","+feed[i]['message']);
-                console.log( feed[i]['updated_time'].substr(0, 9) );
-
+            if( feed[i]['updated_time'].substr(0, 10) == today.today ){
+                if( feed[i]['message']!=null ){
+                    console.log(i+","+feed[i]['message']);
+                }
             }
         }
     });
