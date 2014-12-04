cd8eb1ea4279796fdc1e963db02847cf1e1480a4
streamna <gaoxi1992@hotmail.com>
2014-12-03 21:10:09 -0500

    ggf

--- sentiment/analyse/respond.php
+++ sentiment/analyse/respond.php
@@ -7,18 +7,15 @@
 
     $demo_text = $_GET['analyseText'];
     $response = $alchemyapi->sentiment('text', $demo_text, null);
-    //$respond;
     if ($response['status'] == 'OK') {
 
         if (array_key_exists('score', $response['docSentiment'])) {
             $respond = $response['docSentiment'];
         } else {
             $respond = array("type" => "neutral", "score" => "0.5");
-            //echo json_encode( $respond );
         }
     } else {
         $respond = array("Error" => $response['statusInfo']);
-        //echo json_encode( );
     }
 
     echo json_encode($respond);
@@ -37,28 +34,17 @@
     $score = $respond['score'];
     $mixed = $respond['mixed'];
 
-    if ($type == "positive") {
-        $type = "1";
-    } else if ($type == "negative") {
-        $type = "2";
-    } else { //netural
-        $type = "0";
-    }
-    if ($mixed == null) {
-        $mixed = "0";
-    }
-
     $id = 1;
     //update database
     if($find_db) {
         $query = "UPDATE emotion  SET type='$type',score='$score'  WHERE id='$id' ";
         $result = mysql_query($query);
 
-        if( $result ) {
-            echo "update successfully!";
-        }
+//        if( !$result ) {
+//            $info = array( "error" => "no result" );
+//        }
     }else {
-        $resultFix = array("error" => "databse connection fail");
+        $info = array("error" => "databse connection fail");
     }
 
 
