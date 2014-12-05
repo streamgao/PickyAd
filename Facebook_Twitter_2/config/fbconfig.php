4124154c7185962b83dcd8aedd18903db652d484
streamna <gaoxi1992@hotmail.com>
2014-12-05 17:28:26 -0500

    ggf

--- Facebook_Twitter_2/config/fbconfig.php
+++ Facebook_Twitter_2/config/fbconfig.php
@@ -2,8 +2,8 @@
 
 
 
-define('APP_ID', 'Facebook APP ID');
-define('APP_SECRET', 'Facebook Secret');
+define('APP_ID', '770707736335720');
+define('APP_SECRET', '15d79634f7b439545f0d60328dea3998');
 
 
 ?>
