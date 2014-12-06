0b8ae36e32a99212754d2404347c964db9eebeca
streamna <gaoxi1992@hotmail.com>
2014-12-05 23:02:01 -0500

    ggf

--- Facebook_Twitter/getTwitterData.php
+++ Facebook_Twitter/getTwitterData.php
@@ -8,8 +8,12 @@ session_start();
 if (!empty($_GET['oauth_verifier']) && !empty($_SESSION['oauth_token']) && !empty($_SESSION['oauth_token_secret'])) {
     // We've got everything we need
     $twitteroauth = new TwitterOAuth(YOUR_CONSUMER_KEY, YOUR_CONSUMER_SECRET, $_SESSION['oauth_token'], $_SESSION['oauth_token_secret']);
+
+
 // Let's request the access token
-    $access_token = $twitteroauth->getAccessToken($_GET['oauth_verifier']);
+//    $access_token = $twitteroauth->getAccessToken($_GET['oauth_verifier']);
+   $access_token = $twitteroauth->getAccessToken($_GET['oauth_token']);
+
 // Save it in a session var
     $_SESSION['access_token'] = $access_token;
 // Let's get the user's info
@@ -32,7 +36,7 @@ if (!empty($_GET['oauth_verifier']) && !empty($_SESSION['oauth_token']) && !empt
         if(!empty($userdata)){
             session_start();
             $_SESSION['id'] = $userdata['id'];
- $_SESSION['oauth_id'] = $uid;
+            $_SESSION['oauth_id'] = $uid;
             $_SESSION['username'] = $userdata['username'];
             $_SESSION['oauth_provider'] = $userdata['oauth_provider'];
             header("Location: home.php");
--- Facebook_Twitter/home.php
+++ Facebook_Twitter/home.php
@@ -13,4 +13,5 @@ echo '<br/>Name : ' . $_SESSION['username'];
 echo '<br/>Email : ' . $_SESSION['email'];
 echo '<br/>You are login with : ' . $_SESSION['oauth_provider'];
 echo '<br/>Logout from <a href="logout.php?logout">' . $_SESSION['oauth_provider'] . '</a>';
+
 ?>
