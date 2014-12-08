2e866ada6788fafd06f279f7e45bd76f8eaec4aa
streamna <gaoxi1992@hotmail.com>
2014-12-08 08:18:55 -0500

    g

--- gx/login-twitter.php
+++ gx/login-twitter.php
@@ -5,24 +5,26 @@ $dbg = 0;
 if($_GET['dbg'] == 1) {$dbg=1;}
 
 require_once 'twitter/twitteroauth.php';
+if($dbg) { echo "1<br/>\n";}
 require_once 'config/twconfig.php';
-
+if($dbg) { echo "2<br/>\n";}
 session_start();
-
+if($dbg) { echo "3<br/>\n";}
 $twitteroauth = new TwitterOAuth(YOUR_CONSUMER_KEY, YOUR_CONSUMER_SECRET);
-// Requesting authentication tokens, the parameter is the URL we will be redirected to
+if($dbg) { echo "4<br/>\n";}
+// Requesting authentication tokens, the parameter is the URL we will be redirected to 
 
-//$request_token = $twitteroauth->getRequestToken('http://onlinewebapplication.com/Facebook2/getTwitterData.php');
-//$request_token = $twitteroauth->getRequestToken('http://pickyad-env.elasticbeanstalk.com/twitter/getTwitterData.php');
+//$request_token = $twitteroauth->getRequestToken('http://onlinewebapplication.com/Facebook2/getTwitterData.php'); 
+//$request_token = $twitteroauth->getRequestToken('http://pickyad-env.elasticbeanstalk.com/twitter/getTwitterData.php'); 
 $request_token = $twitteroauth->getRequestToken('http://onlinewebapplication.com/gx/getTwitterData.php');
-
+if($dbg) { echo "5<br/>\n";}
 if($dbg) {
     echo "<pre>\n";
     var_dump($request_token);
     echo "</pre>\n";
 }
 
-// Saving them into the session
+// Saving them into the session 
 $_SESSION['oauth_token'] = $request_token['oauth_token'];
 $_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
 if($dbg) {
@@ -30,14 +32,14 @@ if($dbg) {
     var_dump($_SESSION);
     echo "</pre>\n";
 }
-// If everything goes well..
+// If everything goes well.. 
 if ($twitteroauth->http_code == 200) {
-    // Let's generate the URL and redirect
+    // Let's generate the URL and redirect 
     $url = $twitteroauth->getAuthorizeURL($request_token['oauth_token']);
     if($dbg) { echo "URL: $url\n"; die();}
     header('Location: ' . $url);
 } else {
-    // It's a bad idea to kill the script, but we've got to know when there's an error.
+    // It's a bad idea to kill the script, but we've got to know when there's an error. 
     die('Something wrong happened.');
 }
 ?>
