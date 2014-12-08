9f1afa1af8c750de2e9c8f88240ce444d490383f
streamna <gaoxi1992@hotmail.com>
2014-12-08 06:15:59 -0500

    g

--- Facebook_Twitter/login-twitter.php
+++ Facebook_Twitter/login-twitter.php
@@ -2,19 +2,20 @@
 require_once 'twitter/twitteroauth.php';
 require_once 'config/twconfig.php';
 
-echo "why cannot go to https://twitter.com/oauth/authorize?oauth_token=";
-echo ('why cannot go to');
-echo ("whysa;ldkj");
+//echo "why cannot go to https://twitter.com/oauth/authorize?oauth_token=";
+//echo ('why cannot go to');
+//echo ("whysa;ldkj");
 
 session_start();
 
-$twitteroauth = new TwitterOAuth('YC6UqKwK4fcw8edeyPH56uDzh', 'PpsNoAhXle1Vpq1ppi9Pk9q043j7yNNAUYHfWUwEExTzYbx07r');
-objdump($twitteroauth);
+$twitteroauth = new TwitterOAuth(YOUR_CONSUMER_KEY, YOUR_CONSUMER_SECRET);
 // Requesting authentication tokens, the parameter is the URL we will be redirected to
+//var_dump($twitteroauth);
 //$request_token = $twitteroauth->getRequestToken('http://onlinewebapplication.com/Facebook2/getTwitterData.php');
 $request_token = $twitteroauth->getRequestToken('http://pickyad-env.elasticbeanstalk.com/Facebook_Twitter/getTwitterData.php');
 
-echo($request_token);
+//var_dump($twitteroauth);die();
+//var_dump($request_token); die();
 // Saving them into the session
 
 $_SESSION['oauth_token'] = $request_token['oauth_token'];
@@ -24,6 +25,7 @@ $_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
 if ($twitteroauth->http_code == 200) {
     // Let's generate the URL and redirect
     $url = $twitteroauth->getAuthorizeURL($request_token['oauth_token']);
+    //echo $url; die();
     header('Location: ' . $url);
 } else {
     // It's a bad idea to kill the script, but we've got to know when there's an error.
